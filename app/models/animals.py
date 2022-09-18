41514e42a3b197dc70d1ab90ddb36937db4f5ac0
ponomaREG <e.ponomarev@redmadrobot.com>
2022-09-18 17:09:43 +0300

    a

--- app/models/animals.py
+++ app/models/animals.py
@@ -1,4 +1,5 @@
 from app.models.SqlExecuter import SqlExecuter
+from app.models.users import User
 from app import app
 import os
 
@@ -10,11 +11,14 @@ class Animal:
         for a in animals:
             if a["imageSrc"] is not None:
                 a["imageSrc"] = app.config["URL_PIC"] + a["imageSrc"]
+                
+            a.pop("id_user")
         return animals
 
     @staticmethod
-    def insertAnimal(name, description, image, latitude, longitude):
-        animalId = SqlExecuter.executeQuery("INSERT INTO animal (name, description, latitude, longitude) VALUES ('{}', '{}', '{}', '{}');".format(name, description, latitude, longitude))
+    def insertAnimal(name, description, image, latitude, longitude, token):
+        userId = User.getUserByToken(token)["id"]
+        animalId = SqlExecuter.executeQuery("INSERT INTO animal (name, description, latitude, longitude, id_user) VALUES ('{}', '{}', '{}', '{}', {});".format(name, description, latitude, longitude, userId))
         if (image is not None):
             image.save(app.config["UPLOAD_FOLDER"] + "/" + str(animalId) + ".jpg")
             SqlExecuter.executeQuery("UPDATE animal SET imageSrc = '{}.jpg' WHERE id = {};".format(animalId, animalId))
@@ -26,4 +30,8 @@ class Animal:
         
         if animal["imageSrc"] is not None:
             animal["imageSrc"] = app.config["URL_PIC"] + animal["imageSrc"]
+        userId = animal["id_user"]
+        animal.pop("id_user")
+        userLogin = SqlExecuter.getRowPacked("SELECT * FROM user where id = {};".format(userId))["login"]
+        animal["userLogin"] = userLogin
         return animal
--- app/models/users.py
+++ app/models/users.py
@@ -1,3 +1,4 @@
+from lib2to3.pgen2 import token
 from operator import truediv
 from app.models.SqlExecuter import SqlExecuter
 from app.models.token import Token
@@ -5,18 +6,23 @@ from app.models.token import Token
 class User:
 
     @staticmethod
-    def authenticateUser(phone, code):
+    def authenticateUser(phone, code, login):
         newToken = Token.generateToken()
         existsPhone = SqlExecuter.getRowsPacked("SELECT * FROM user WHERE phone = '{}';".format(phone))
         if (len(existsPhone) > 0):
             SqlExecuter.executeQuery("UPDATE user SET token = '{}' WHERE phone = '{}';".format(newToken, phone))
         else:
-            SqlExecuter.executeQuery("INSERT INTO user (phone, token) VALUES('{}', '{}');".format(phone, newToken))
+            SqlExecuter.executeQuery("INSERT INTO user (phone, token, login) VALUES('{}', '{}', '{}');".format(phone, newToken, login))
         return newToken
 
     @staticmethod
     def sendOtpCode(phone):
         pass
+
+    @staticmethod
+    def getUserByToken(token):
+        userRow = SqlExecuter.getRowsPacked("SELECT * FROM user WHERE token = '{}';".format(token))
+        return userRow[0]
 
     @staticmethod
     def checkUserToken(token):
